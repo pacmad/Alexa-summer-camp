96ab8ff618e55941a0bcaec2209cb5cd92c30cd2
zoema-0928 <52638980+zoema-0928@users.noreply.github.com>
2019-07-08 09:47:47 +0800

    Create model.js

---
+++ plan my trip/model.js
@@ -0,0 +1,42 @@
+{
+    "interactionModel": {
+        "languageModel": {
+            "invocationName": "plan my trip",
+            "intents": [
+                {
+                    "name": "AMAZON.CancelIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.HelpIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.StopIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.NavigateHomeIntent",
+                    "samples": []
+                },
+                {
+                    "name": "TripIntnet",
+                    "slots": [],
+                    "samples": [
+                        "start planing",
+                        "plan it"
+                    ]
+                },
+                {
+                    "name": "AMAZON.YesIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.NoIntent",
+                    "samples": []
+                }
+            ],
+            "types": []
+        }
+    }
+}
